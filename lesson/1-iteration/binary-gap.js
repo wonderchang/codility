1dc713ed976bc54e4b2f1a2271bca7c8cb65ad39
Wonder Chang <iwonder.tw@gmail.com>
2016-02-25 00:21:50 +0800

    Add binaryGap

--- lesson/1-iteration/binary-gap.js
+++ lesson/1-iteration/binary-gap.js
@@ -1,21 +1,17 @@
-
-console.log(solution(9));
-console.log(solution(529));
-console.log(solution(20));
-console.log(solution(15));
-console.log(solution(1041));
-
 function solution(N) {
-  var l = 0;
-  var i = 0;
-  var L = 0;
   N = N.toString(2);
+  var g = 0; // Temp gap size
+  var G = 0; // Largest gap size
+  var i = 0;
+  // Eliminate 0 head
   while(N[i++] === '0') continue;
+  // Start process when encoutering 1
   while(N[i] !== undefined) {
     if(N[i++] === '1') {
-      if(l > L) L = l;
-      l = 0
-    } else l++;
+      // Update when it is larger
+      if(g > G) G = g;
+      g = 0;
+    } else g++;
   }
-  return L;
+  return G;
 }
